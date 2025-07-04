a7b7eeadd6f7f7ff6347711f9beda69dbbca4918
johnny1607 <jonaib13@hotmail.com>
2025-07-04 10:17:39 -0700

    Create Multx.java

---
+++ ArbolSintactico/Multx.java
@@ -0,0 +1,11 @@
+package ArbolSintactico;
+
+public class Multx extends Expx {
+    private Idx s1;
+    private Idx s2;
+
+    public Multx(Idx st1, Idx st2) {
+        s1 = st1;
+        s2 = st2;
+    }
+}
